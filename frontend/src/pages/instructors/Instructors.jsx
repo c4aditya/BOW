8cdb5100abc9be2ccbf0781507040c706233a881
Aditya Singh <singhas1418@gmail.com>
2025-03-25 14:35:54 +0530

    adding instructors in about us page

--- frontend/src/pages/instructors/Instructors.jsx
+++ frontend/src/pages/instructors/Instructors.jsx
@@ -1,47 +1,59 @@
 
 import "./instructors.css"
+import { CiLinkedin } from "react-icons/ci";
+import { CiInstagram } from "react-icons/ci";
+import { CiTwitter } from "react-icons/ci";
 function Instructors() {
 
     const ourInstructors = [
         {
             id: 1,
-            name: "Roshan",
+            name: "Roshan Rashtogi",
             image: "https://acelearningtraining.com/wp-content/uploads/2024/04/successful-businessman-1414x1536.jpg"
         },
 
         {
             id: 2,
-            name: "AS",
+            name: "Arvind Chauchan",
             image: "https://acelearningtraining.com/wp-content/uploads/2024/04/successful-businessman-1414x1536.jpg"
         },
 
         {
             id: 3,
-            name: "Ram",
+            name: "Raju Rastogii",
             image: "https://acelearningtraining.com/wp-content/uploads/2024/04/successful-businessman-1414x1536.jpg"
         },
     ]
 
     return (
         <div>
+            
             <div className="main-image-our-instructors">
                 {ourInstructors.map(({ id, name, image }) => (
 
-                   <div>
-                    <div className="image-our-instructors">
-                        <img src={image} />
+                    <div>
+                        <div className="image-our-instructors">
+                            <img src={image} />
 
 
-                        <div className="more-about-instructors">
-                        <h1>{name}</h1>
+                            <div className="more-about-instructors-name">
+                                <p>{name}</p>
+                            </div>
+
+
+                            <div className="social-meadia-links-instructors">
+                            <ul>
+                                <li><a href="https://linkedie.com"><CiLinkedin /></a></li>
+                                <li><a href="https://facebook.com"> <CiInstagram/> </a></li>
+                                <li><a href="https://twitter.com"><CiTwitter/></a></li>
+                                
+                            </ul>
+                            </div>
+
                         </div>
 
+                        
                     </div>
-
-                 
-
-
-                        </div>
 
 
 
